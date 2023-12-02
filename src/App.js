acb01200b697d5499921345dbb0a10aae19a41c9
bonisiweGabela <142170923+bonisiweGabela@users.noreply.github.com>
2023-12-02 22:57:08 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
+import React from "react";
+
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by Bonisiwe Gabela and is {""}
+        <a
+          href="https://github.com/bonisiweGabela/react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>{" "}
+    </div>
+  );
 }
-
-export default App;
