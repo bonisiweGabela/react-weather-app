928dd52bd3f94138bfd0ff7f93606b90a9d29e76
bonisiweGabela <142170923+bonisiweGabela@users.noreply.github.com>
2023-12-04 10:16:24 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project was coded by Bonisiwe Gabela and is {""}
-        <a
-          href="https://github.com/bonisiweGabela/react-weather-app"
-          target="_blank"
-          rel="nonopener noreferrer"
-        >
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
           {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>{" "}
+          This project was coded by Bonisiwe Gabela and is {""}
+          <a
+            href="https://github.com/bonisiweGabela/react-weather-app"
+            target="_blank"
+            rel="nonopener noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>{" "}
+      </div>
     </div>
   );
 }
