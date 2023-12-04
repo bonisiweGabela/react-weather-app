b00f8dcd6a129fc7c0dc149cb7c6a8bdef6b39b7
bonisiweGabela <142170923+bonisiweGabela@users.noreply.github.com>
2023-12-04 11:05:09 +0200

    Updated weather.js

---
+++ src/Weather.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1> New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <img
+          src="https://ss1.gstatic.com/onebox/weather/64/partly_cloudy.png"
+          alt="Mostly Cloudy"
+        />
+        6C
+      </div>
+      <div className="col-6">
+        <ul>
+          <li> Precipitation </li>
+          <li> Humidity</li>
+          <li> Wind: 13 km/h %</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
